ae217e248e14dc9a16b0e5829ba4fd642cbc7777
Xavier <isiomastewart@gmail.com>
2022-02-09 22:18:49 +0100

    Mercy Chinwo - Baby Song

--- Server.ts
+++ Server.ts
@@ -1,19 +1,34 @@
 import { createServer, Socket } from "net";
 import Handler from "./Worker";
+import { readFileSync } from "fs";
 
 export const PORT = /**Number.parseInt(process.env.PORT) || */1025;
+
 
 let noOfConn = 0;
 let clientSocket: Socket;
 let dataPort: number;
+let version: string;
+
+const file = (filePath: string): string => { 
+  return readFileSync(filePath, "utf8");
+}
+
 
 const server = createServer(socket => {
   //socket.pipe(process.stdout);
   //console.log(socket);
+  version = JSON.parse(file("./package.json")).version;
   
-  socket.write("\nWelcome to the Álviss Raghnall Foundation😏  FTP Server. Please enter a command. \n");
-  socket.pipe(socket);
+  socket.write(`\n220  Welcome to the Álviss Raghnall Foundation😏  FTP Server.\nversion: ${version}\n${new Date()}\n`);
+  // socket.pipe(process.stdout);
+  // process.stdout.write("x");
+  // console.log(process.stdout);
   clientSocket = socket;
+  
+  // socket.on("data", chunk => {
+    // console.log(chunk, typeof chunk);
+  // });
   
 })  /**
  Creates a new instance of the Handler class on every client connection.
@@ -23,11 +38,14 @@ const server = createServer(socket => {
     if(err) throw err;
     noOfConn = count;
     console.log(noOfConn);
+    
   });
+
   
-  
-  dataPort = PORT + noOfConn + 1;
+  dataPort = PORT + ++noOfConn;
   const handler = new Handler(clientSocket, dataPort);
+  
+  await handler.start();
     //if(err) throw err;
     //return count;
   //console.log('connected! outside!!')
--- Worker.ts
+++ Worker.ts
@@ -1,7 +1,87 @@
-import { Socket } from "net";
+import type { Socket } from "net";
+
+enum userStatus {
+  NOTLOGGEDIN, ENTEREDUSERNAME, LOGGEDIN, ANONYMOUS
+}
 
 export default class Handler {
-  constructor(private clientSock: Socket, readonly dataPort: number){
+  private trfMode: "active" | "passive";
+  private endCommands: boolean = false;
+  private userLoggedInStatus: userStatus = userStatus.NOTLOGGEDIN;
+  private debugMode = true;
+  
+  constructor(private clientSock: Socket, readonly dataPort: number) {
+    this.trfMode = "passive";
+  }
+  
+  /**
+   * called for every client console input. Receives 
+   * client commands (as well as args), and calls 
+   * commander method when ${endCommands} remains untrue, otherwise, it emits a close event. (????)
+   * 
+  */
+  private newData(): void {
+    //if (!this.endCommands) {
+    this.clientSock.on("data", chunk => {
+      console.log(chunk, chunk.toString(), this.dataPort);
+      if(!this.endCommands){
+        this.commander(chunk.toString());
+      }
+    });
+    //}
+    //console.log(this.clientSock);
     
   }
+  
+  /**
+   * Parse client input, from buffer, into valid strings,
+   * and pushes handling of individual commands to
+   * various handlers availale.
+  */
+  
+  private commander(cmdargs: string) {
+    console.log("===========");
+    let [cmd, ...args] = cmdargs.split(" ");
+    cmd = cmd.toUpperCase();
+    this.debugPrintOut(`Command: ${cmd} \n Args: ${typeof args}`);
+    switch(cmd){
+      case "USER":
+        this.handleUserComm(args);
+        break;
+        
+      //case 
+    }
+  }
+  /**
+  For printing out information in debug mode. 
+  */
+  private debugPrintOut(msg: string): void {
+    if(this.debugMode) {
+      console.log(`Client Connection: ${this.dataPort - 1025} \n ${msg}`)
+    }
+  }
+  
+  /**
+   * Method for handling user login upon connection to the ÁR ftp server. 
+   * Accepts array of string arguments passed via the command line, as arguments.
+   * 
+  */
+  
+  private handleUserComm(args: string | string[]) {
+    
+  }
+  
+  /**
+  
+  */
+  async start() {
+    try {
+      this.newData();
+    } catch(err: unknown){
+      const error = <Error> err;
+      console.error(error);
+      process.exit(1);
+    }
+  }
+
 }
